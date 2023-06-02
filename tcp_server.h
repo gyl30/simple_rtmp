4f9cf673ba805bb2cac843d061f0a20ca27c6093
gyl30 <im.ggyyll@gmail.com>
2023-06-02 22:52:13 +0800

    添加 server name

--- tcp_server.h
+++ tcp_server.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 #include <boost/asio.hpp>
 #include "execution.h"
@@ -15,14 +16,14 @@ template <typename Session>
 class tcp_server : public std::enable_shared_from_this<tcp_server<Session>>
 {
    public:
-    tcp_server(uint16_t port, executors::executor &io, executors &pool) : port_(port), acceptor_(io), pool_(pool)
+    tcp_server(uint16_t port, std::string name, executors::executor &io, executors &pool) : port_(port), name_(std::move(name)), acceptor_(io), pool_(pool)
     {
-        LOG_INFO("tcp server :{} create", port_);
+        LOG_INFO("{} server :{} create", name_, port_);
     }
 
     ~tcp_server()
     {
-        LOG_INFO("tcp server :{} destroy", port_);
+        LOG_INFO("{} server :{} destroy", name_, port_);
     }
     tcp_server(const tcp_server &)            = delete;
     tcp_server(tcp_server &&)                 = delete;
@@ -52,13 +53,13 @@ class tcp_server : public std::enable_shared_from_this<tcp_server<Session>>
         boost::asio::ip::address addr = boost::asio::ip::make_address(ip, ec);
         if (ec)
         {
-            LOG_ERROR("tcp server address :{} error {}", port_, ec.message());
+            LOG_ERROR("{} server address :{} error {}", name_, port_, ec.message());
             return;
         }
         acceptor.open(boost::asio::ip::tcp::v4(), ec);
         if (ec)
         {
-            LOG_ERROR("tcp server :{} open error {}", port_, ec.message());
+            LOG_ERROR("{} server :{} open error {}", name_, port_, ec.message());
             return;
         }
         int one  = 1;
@@ -71,31 +72,31 @@ class tcp_server : public std::enable_shared_from_this<tcp_server<Session>>
         int ret = setsockopt(acceptor.native_handle(), SOL_SOCKET, flag, &one, sizeof(one));
         if (ret == -1)
         {
-            LOG_WARN("tcp server :{} setsockopt error {}", port_, errno_to_str());
+            LOG_WARN("{} server :{} setsockopt error {}", name_, port_, errno_to_str());
         }
         acceptor.bind(boost::asio::ip::tcp::endpoint(addr, port_), ec);
         if (ec)
         {
-            LOG_ERROR("tcp server :{} bind error {}", port_, ec.message());
+            LOG_ERROR("{} server :{} bind error {}", name_, port_, ec.message());
             return;
         }
         acceptor.listen(boost::asio::ip::tcp::socket::max_listen_connections, ec);
         if (ec)
         {
-            LOG_ERROR("tcp server :{} listen error {}", port_, ec.message());
+            LOG_ERROR("{} server :{} listen error {}", name_, port_, ec.message());
             return;
         }
         acceptor_ = std::move(acceptor);
-        LOG_INFO("tcp server :{} listen", port_);
+        LOG_INFO("{} server :{} listen", name_, port_);
     }
     void do_accept()
     {
         if (!acceptor_.is_open())
         {
-            LOG_ERROR("tcp server :{} accept error", port_);
+            LOG_ERROR("{} server :{} accept error", name_, port_);
             return;
         }
-        LOG_INFO("tcp server :{} accept count ", port_, count_++);
+        LOG_INFO("{} server :{} accept count {}", name_, port_, count_++);
         session_  = std::make_shared<Session>(pool_.get_executor());
         auto self = tcp_server<Session>::shared_from_this();
         acceptor_.async_accept(session_->socket(), [this, self](boost::system::error_code ec) { on_accept(ec); });
@@ -104,12 +105,12 @@ class tcp_server : public std::enable_shared_from_this<tcp_server<Session>>
     {
         if (ec == boost::system::errc::operation_canceled)
         {
-            LOG_WARN("tcp server :{} accept cancel", port_);
+            LOG_WARN("{} server :{} accept cancel", name_, port_);
             return;
         }
         if (ec)
         {
-            LOG_ERROR("tcp server :{} accept error {}", port_, ec.message());
+            LOG_ERROR("{} server :{} accept error {}", name_, port_, ec.message());
             return;
         }
         session_->start();
@@ -119,6 +120,7 @@ class tcp_server : public std::enable_shared_from_this<tcp_server<Session>>
    private:
     uint16_t port_  = 0;
     uint32_t count_ = 0;
+    std::string name_;
     boost::asio::ip::tcp::acceptor acceptor_;
     std::shared_ptr<Session> session_ = nullptr;
     simple_rtmp::executors &pool_;
