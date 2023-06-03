b6d90f69204114f7ad0ab3e701082f38c8c1d3e2
gyl30 <im.ggyyll@gmail.com>
2023-06-03 22:13:34 +0800

    添加 rtmp source

---
+++ rtmp_source.cc
@@ -0,0 +1,29 @@
+#include "rtmp_source.h"
+#include "rtmp_demuxer.h"
+
+using simple_rtmp::rtmp_source;
+using simple_rtmp::rtmp_demuxer;
+
+rtmp_source::rtmp_source() : demuxer_(std::make_shared<rtmp_demuxer>()){};
+
+void rtmp_source::on_frame(const frame_buffer::ptr& frame, const boost::system::error_code& ec)
+{
+    if (ec)
+    {
+        return;
+    }
+    if (ch_)
+    {
+        ch_->write(frame, ec);
+    }
+}
+
+void rtmp_source::write(const frame_buffer::ptr& frame, const boost::system::error_code& ec)
+{
+    demuxer_->write(frame, ec);
+}
+
+void simple_rtmp::rtmp_source::set_channel(const channel::ptr& ch)
+{
+    ch_ = ch;
+}
---
+++ rtmp_source.h
@@ -0,0 +1,37 @@
+#ifndef SIMPLE_RTMP_RTMP_SOURCE_H
+#define SIMPLE_RTMP_RTMP_SOURCE_H
+
+#include <string>
+#include <utility>
+#include <memory>
+#include <boost/system/error_code.hpp>
+#include "frame_buffer.h"
+#include "rtmp_demuxer.h"
+#include "channel.h"
+
+namespace simple_rtmp
+{
+
+class rtmp_source : public std::enable_shared_from_this<rtmp_source>
+{
+   public:
+    using prt = std::shared_ptr<rtmp_source>;
+
+   public:
+    rtmp_source();
+
+   public:
+    void write(const frame_buffer::ptr& frame, const boost::system::error_code& ec);
+    void set_channel(const channel::ptr& ch);
+
+   private:
+    void on_frame(const frame_buffer::ptr& frame, const boost::system::error_code& ec);
+
+   private:
+    channel::ptr ch_;
+    rtmp_demuxer::prt demuxer_;
+};
+
+}    // namespace simple_rtmp
+
+#endif
