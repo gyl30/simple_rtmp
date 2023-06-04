57d7a247e6d7f73ef9a726b5fcc4ec4a4e06fe26
gyl30 <im.ggyyll@gmail.com>
2023-06-05 07:34:19 +0800

    添加 decoder

---
+++ rtmp_decoder.h
@@ -0,0 +1,24 @@
+#ifndef SIMPLE_RTMP_RTMP_DECODER_H
+#define SIMPLE_RTMP_RTMP_DECODER_H
+
+#include <string>
+#include <functional>
+#include "frame_buffer.h"
+#include "channel.h"
+
+namespace simple_rtmp
+{
+class rtmp_decoder
+{
+   public:
+    virtual ~rtmp_decoder();
+
+   public:
+    virtual void write(const frame_buffer::ptr &frame);
+    virtual void on_codec(const std::function<void(int)> &codec_cb);
+    virtual void set_output(const channel::ptr &ch);
+};
+
+}    // namespace simple_rtmp
+
+#endif
