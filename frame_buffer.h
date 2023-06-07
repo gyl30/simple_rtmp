b88f19f48edfe5f86e589f25817476c8a13e1b82
gyl30 <im.ggyyll@gmail.com>
2023-06-07 22:39:53 +0800

    添加 media

--- frame_buffer.h
+++ frame_buffer.h
@@ -12,6 +12,7 @@ namespace simple_rtmp
 struct frame_buffer
 {
     using ptr     = std::shared_ptr<frame_buffer>;
+    int32_t media = 0;
     int32_t codec = 0;
     int32_t flag  = 0;
     int64_t pts   = 0;
