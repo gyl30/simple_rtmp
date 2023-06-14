e0f3c927b19e3c9d41b9fe4fe2d35f4d3b88e570
gyl30 <im.ggyyll@gmail.com>
2023-06-14 22:23:41 +0800

    添加 tcp connection

---
+++ tcp_connection.cc
@@ -0,0 +1,136 @@
+#include "tcp_connection.h"
+#include "socket.h"
+#include "log.h"
+
+using simple_rtmp::tcp_connection;
+
+tcp_connection::tcp_connection(simple_rtmp::executors::executor& ex) : ex_(ex)
+{
+    LOG_DEBUG("create {}", static_cast<void*>(this));
+}
+
+tcp_connection::~tcp_connection()
+{
+    LOG_DEBUG("destroy {}", static_cast<void*>(this));
+}
+
+boost::asio::ip::tcp::socket& tcp_connection::socket()
+{
+    return socket_;
+}
+
+void tcp_connection::start()
+{
+    local_addr_  = get_socket_local_address(socket_);
+    remote_addr_ = get_socket_remote_address(socket_);
+    LOG_DEBUG("start {} <--> {}", local_addr_, remote_addr_);
+    do_read();
+}
+
+void tcp_connection::shutdown()
+{
+    LOG_DEBUG("shutdown {}", static_cast<void*>(this));
+    ex_.post(std::bind(&tcp_connection::safe_shutdown, shared_from_this()));
+}
+
+void tcp_connection::safe_shutdown()
+{
+    if (socket_.is_open())
+    {
+        LOG_DEBUG("safe shutdown {} <--> {}", local_addr_, remote_addr_);
+        boost::system::error_code ec;
+        socket_.close(ec);
+    }
+    else
+    {
+        LOG_DEBUG("safe shutdown {}", static_cast<void*>(this));
+    }
+}
+
+void tcp_connection::set_read_cb(const read_cb& cb)
+{
+    read_cb_ = cb;
+}
+
+void tcp_connection::set_write_cb(const write_cb& cb)
+{
+    write_cb_ = cb;
+}
+
+void tcp_connection::do_read()
+{
+    socket_.async_read_some(boost::asio::buffer(buf_, kBufSize), std::bind(&tcp_connection::on_read, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+}
+
+void tcp_connection::on_read(const boost::system::error_code& ec, std::size_t bytes)
+{
+    auto frame = std::make_shared<frame_buffer>();
+    if (!ec)
+    {
+        frame->append(buf_, bytes);
+    }
+
+    if (read_cb_)
+    {
+        read_cb_(ec, frame);
+    }
+    else if (ec)
+    {
+        LOG_DEBUG("{} <--> {} read failed {}", local_addr_, remote_addr_, ec.message());
+        shutdown();
+        return;
+    }
+
+    LOG_TRACE("{} <--> {} read {} bytes", local_addr_, remote_addr_, bytes);
+    do_read();
+}
+
+void tcp_connection::write_frame(simple_rtmp::frame_buffer::ptr& frame)
+{
+    ex_.post(std::bind(&tcp_connection::safe_write_frame, shared_from_this(), frame));
+}
+
+void tcp_connection::safe_write_frame(simple_rtmp::frame_buffer::ptr& frame)
+{
+    write_queue_.push_back(frame);
+    safe_do_write();
+}
+
+void tcp_connection::safe_do_write()
+{
+    if (!writing_queue_.empty())
+    {
+        return;
+    }
+    if (write_queue_.empty())
+    {
+        return;
+    }
+    auto self = shared_from_this();
+    writing_queue_.swap(write_queue_);
+    std::vector<boost::asio::const_buffer> bufs;
+    bufs.reserve(writing_queue_.size());
+    for (const auto& frame : writing_queue_)
+    {
+        bufs.emplace_back(boost::asio::buffer(frame->payload));
+    }
+    boost::asio::async_write(socket_, bufs, std::bind(&tcp_connection::safe_on_write, self, std::placeholders::_1, std::placeholders::_2));
+}
+
+void tcp_connection::safe_on_write(const boost::system::error_code& ec, std::size_t bytes)
+{
+    writing_queue_.clear();
+
+    if (write_cb_)
+    {
+        write_cb_(ec, bytes);
+    }
+    else if (ec)
+    {
+        LOG_DEBUG("{} <--> {} write failed {}", local_addr_, remote_addr_, ec.message());
+        shutdown();
+        return;
+    }
+    LOG_TRACE("{} <--> {} write {} bytes", local_addr_, remote_addr_, bytes);
+    safe_do_write();
+}
---
+++ tcp_connection.h
@@ -0,0 +1,54 @@
+#ifndef SIMPLE_RTMP_TCP_CONNECTION_H
+#define SIMPLE_RTMP_TCP_CONNECTION_H
+
+#include <memory>
+#include "execution.h"
+#include "channel.h"
+#include "frame_buffer.h"
+
+namespace simple_rtmp
+{
+class tcp_connection : public std::enable_shared_from_this<tcp_connection>
+{
+   public:
+    explicit tcp_connection(simple_rtmp::executors::executor& ex);
+    ~tcp_connection();
+
+   public:
+    void start();
+    void shutdown();
+    boost::asio::ip::tcp::socket& socket();
+
+   public:
+    using read_cb  = std::function<void(boost::system::error_code, const simple_rtmp::frame_buffer::ptr&)>;
+    using write_cb = std::function<void(boost::system::error_code, std::size_t)>;
+    void set_read_cb(const read_cb& cb);
+    void set_write_cb(const write_cb& cb);
+    void write_frame(simple_rtmp::frame_buffer::ptr& frame);
+
+   private:
+    void do_read();
+    void on_read(const boost::system::error_code& ec, std::size_t bytes);
+    void do_write(const frame_buffer::ptr& frame);
+    void safe_write_frame(simple_rtmp::frame_buffer::ptr& frame);
+    void safe_do_write();
+    void safe_on_write(const boost::system::error_code& ec, std::size_t bytes);
+    void safe_shutdown();
+
+   private:
+    std::string local_addr_;
+    std::string remote_addr_;
+
+    const static int kBufSize = 64 * 1024;
+    uint8_t buf_[kBufSize]    = {0};
+    read_cb read_cb_          = nullptr;
+    write_cb write_cb_        = nullptr;
+    simple_rtmp::executors::executor& ex_;
+    boost::asio::ip::tcp::socket socket_{ex_};
+    std::vector<frame_buffer::ptr> write_queue_;
+    std::vector<frame_buffer::ptr> writing_queue_;
+};
+
+}    // namespace simple_rtmp
+
+#endif
