625a30f2470a501dd25b7a8d063187c06df18c60
gyl30 <im.ggyyll@gmail.com>
2023-06-19 09:07:51 +0800

    修改错误信息获取

--- error.cc
+++ error.cc
@@ -1,13 +1,19 @@
 #include <cstdio>
+#include <cstring>
 #include "error.h"
 
 std::string errno_to_str()
 {
     char buf[BUFSIZ] = {0};
-    int ret          = strerror_r(errno, buf, sizeof buf);
+#ifdef _GNU_SOURCE
+    char* ret = strerror_r(errno, buf, sizeof buf);
+    return ret;
+#else
+    int ret = strerror_r(errno, buf, sizeof buf);
     if (ret != 0)
     {
         return "unknown error";
     }
     return buf;
+#endif
 }
