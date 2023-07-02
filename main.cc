eaf1f847b8318e358bdc1cbfa1e725f7f55fa16c
gyl30 <im.ggyyll@gmail.com>
2023-07-02 21:42:36 +0800

    使用 rtsp session

--- main.cc
+++ main.cc
@@ -8,15 +8,19 @@
 #include "log.h"
 #include "rtmp_publish_session.h"
 #include "rtmp_forward_session.h"
+#include "rtsp_forward_session.h"
 
 using simple_rtmp::rtmp_publish_session;
 using simple_rtmp::rtmp_forward_session;
+using simple_rtmp::rtsp_forward_session;
 using simple_rtmp::tcp_server;
 
 static const std::string kRtmpServerName = "rtmp publish";
 static const std::string kRtmpForwardServerName = "rtmp forward";
+static const std::string kRtspForwardServerName = "rtsp forward";
 static const uint16_t kRtmpPublishPort = 1935;
 static const uint16_t kRtmpForwardPort = 1936;
+static const uint16_t kRtspForwardPort = 8554;
 
 int main(int argc, char* argv[])
 {
@@ -63,6 +67,7 @@ int main(int argc, char* argv[])
 
     std::make_shared<tcp_server<rtmp_publish_session>>(kRtmpPublishPort, kRtmpServerName, exs.get_executor(), exs)->run();
     std::make_shared<tcp_server<rtmp_forward_session>>(kRtmpForwardPort, kRtmpForwardServerName, exs.get_executor(), exs)->run();
+    std::make_shared<tcp_server<rtsp_forward_session>>(kRtspForwardPort, kRtspForwardServerName, exs.get_executor(), exs)->run();
 
     exs.run();
 
