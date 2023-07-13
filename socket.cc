b3bcdc6aa953118e5abdf42ca53320c54ce39921
gyl30 <im.ggyyll@gmail.com>
2023-07-13 21:27:07 +0800

    完善 socket 方法

--- socket.cc
+++ socket.cc
@@ -3,8 +3,8 @@
 
 namespace simple_rtmp
 {
-
-std::string get_endpoint_address(const boost::asio::ip::udp::endpoint& ed)
+template <typename Ed>
+std::string get_endpoint_address_(const Ed& ed)
 {
     boost::system::error_code ec;
     const std::string ip = ed.address().to_string(ec);
@@ -14,18 +14,106 @@ std::string get_endpoint_address(const boost::asio::ip::udp::endpoint& ed)
     }
     const uint16_t port = ed.port();
     return ip + ":" + std::to_string(port);
+}
+
+template <typename Socket>
+static std::string get_socket_local_ip_(Socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.local_endpoint(ec);
+    if (ec)
+    {
+        return "";
+    }
+    std::string ip = ed.address().to_string(ec);
+    if (ec)
+    {
+        return "";
+    }
+    return ip;
+}
+template <typename Socket>
+static uint16_t get_socket_local_port_(Socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.local_endpoint(ec);
+    if (ec)
+    {
+        return 0;
+    }
+    return ed.port();
+}
+//
+template <typename Socket>
+static std::string get_socket_remote_ip_(Socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.remote_endpoint(ec);
+    if (ec)
+    {
+        return "";
+    }
+    std::string ip = ed.address().to_string(ec);
+    if (ec)
+    {
+        return "";
+    }
+    return ip;
+}
+template <typename Socket>
+static uint16_t get_socket_remote_port_(Socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.remote_endpoint(ec);
+    if (ec)
+    {
+        return 0;
+    }
+    return ed.port();
+}
+std::string get_socket_local_ip(boost::asio::ip::udp::socket& socket)
+{
+    return get_socket_local_ip_(sock);
+}
+
+uint16_t get_socket_local_port(boost::asio::ip::udp::socket& socket)
+{
+    return get_socket_local_port_(socket);
+}
+
+std::string get_socket_remote_ip(boost::asio::ip::udp::socket& socket)
+{
+    return get_socket_remote_ip_(socket);
+}
+uint16_t get_socket_remote_port(boost::asio::ip::udp::socket& socket)
+{
+    return get_socket_remote_port_(socket);
+}
+std::string simple_rtmp::get_socket_local_ip(boost::asio::ip::tcp::socket& socket)
+{
+    return get_socket_local_ip_(socket);
+}
+uint16_t simple_rtmp::get_socket_local_port(boost::asio::ip::tcp::socket& socket)
+{
+    return get_socket_local_port_(socket);
+}
+std::string simple_rtmp::get_socket_remote_ip(boost::asio::ip::tcp::socket& socket)
+{
+    return get_socket_remote_ip_(socket);
+}
+uint16_t simple_rtmp::get_socket_remote_port(boost::asio::ip::tcp::socket& socket)
+{
+    return get_socket_remote_port_(socket);
 }
 std::string get_endpoint_address(const boost::asio::ip::tcp::endpoint& ed)
 {
-    boost::system::error_code ec;
-    const std::string ip = ed.address().to_string(ec);
-    if (ec)
-    {
-        return "";
-    }
-    const uint16_t port = ed.port();
-    return ip + ":" + std::to_string(port);
+    return get_endpoint_address_(ed);
 }
+std::string get_endpoint_address(const boost::asio::ip::udp::endpoint& ed)
+{
+    return get_endpoint_address_(ed);
+}
+
 std::string get_socket_remote_address(boost::asio::ip::tcp::socket& socket)
 {
     boost::system::error_code ec;
@@ -56,32 +144,7 @@ std::string get_socket_local_address(boost::asio::ip::udp::socket& socket)
     }
     return get_endpoint_address(ed);
 }
-std::string get_socket_local_ip(boost::asio::ip::udp::socket& socket)
-{
-    boost::system::error_code ec;
-    auto ed = socket.local_endpoint(ec);
-    if (ec)
-    {
-        return "";
-    }
-    const std::string ip = ed.address().to_string(ec);
-    if (ec)
-    {
-        return "";
-    }
-    return ip;
-}
-uint16_t get_socket_local_port(boost::asio::ip::udp::socket& socket)
-{
-    boost::system::error_code ec;
-    auto ed = socket.local_endpoint(ec);
-    if (ec)
-    {
-        return 0;
-    }
-    return ed.port();
-}
-std::string get_socket_remote_ip(boost::asio::ip::udp::socket& socket)
+std::string get_socket_remote_address(boost::asio::ip::udp::socket& socket)
 {
     boost::system::error_code ec;
     auto ed = socket.remote_endpoint(ec);
@@ -89,23 +152,9 @@ std::string get_socket_remote_ip(boost::asio::ip::udp::socket& socket)
     {
         return "";
     }
-    const std::string ip = ed.address().to_string(ec);
-    if (ec)
-    {
-        return "";
-    }
-    return ip;
+    return get_endpoint_address(ed);
 }
-uint16_t get_socket_remote_port(boost::asio::ip::udp::socket& socket)
-{
-    boost::system::error_code ec;
-    auto ed = socket.remote_endpoint(ec);
-    if (ec)
-    {
-        return 0;
-    }
-    return ed.port();
-}
+
 boost::asio::ip::tcp::socket change_socket_io_context(boost::asio::ip::tcp::socket sock, boost::asio::io_context& io)
 {
     std::string local = get_socket_local_address(sock);
