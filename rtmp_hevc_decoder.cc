d767aab38e8825990f59aaecf188caf1cbc19076
gyl30 <im.ggyyll@gmail.com>
2023-08-07 22:35:54 +0800

    完善 rtmp hevc decoder

--- rtmp_hevc_decoder.cc
+++ rtmp_hevc_decoder.cc
@@ -1,5 +1,6 @@
 #include <utility>
 #include "rtmp_hevc_decoder.h"
+#include "rtmp_codec.h"
 #include "flv-header.h"
 #include "mpeg4-hevc.h"
 #include "execution.h"
@@ -12,11 +13,19 @@ enum
     end_of_sequence = 2,
 };
 
+#define H265_NAL_BLA_W_LP 16
+#define H265_NAL_RSV_IRAP 23
+#define H265_NAL_VPS 32
+#define H265_NAL_SPS 33
+#define H265_NAL_PPS 34
+#define H265_NAL_AUD 35    // Access unit delimiter
+
 using simple_rtmp::rtmp_h265_decoder;
 
 struct rtmp_h265_decoder::args
 {
     struct mpeg4_hevc_t hevc;
+    int vps_sps_pps_flag = 0;
 };
 
 rtmp_h265_decoder::rtmp_h265_decoder(std::string id) : id_(std::move(id))
@@ -81,6 +90,72 @@ void rtmp_h265_decoder::write(const frame_buffer::ptr& frame, boost::system::err
     demuxer_avpacket(data, bytes, frame->pts(), video.cts, video.keyframe == 1 ? 1 : 0);
 }
 
+static int h265_vps_sps_pps_size(const struct mpeg4_hevc_t* hevc)
+{
+    int i, n = 0;
+    for (i = 0; i < hevc->numOfArrays; i++) n += hevc->nalu[i].bytes + 4;
+    return n;
+}
+
 void rtmp_h265_decoder::demuxer_avpacket(const uint8_t* data, size_t bytes, int64_t timestamp, int64_t cts, int keyframe)
 {
+    size_t offset = 0;
+    const uint8_t* data_offset = data;
+    const uint8_t* end = data + bytes;
+
+    while (true)
+    {
+        int length = 0;
+        for (int i = 0; i < args_->hevc.lengthSizeMinusOne + 1; i++)
+        {
+            length = (length << 8) + (const_cast<uint8_t*>(data_offset))[i];
+        }
+        if (args_->hevc.lengthSizeMinusOne == 0 || (length == 1 && (args_->hevc.lengthSizeMinusOne == 2 || args_->hevc.lengthSizeMinusOne == 3)))
+        {
+            // annexb
+            break;
+        }
+        offset += args_->hevc.lengthSizeMinusOne + 1;
+        if (length < 0 || data_offset + length > end)
+        {
+            break;
+        }
+        int nalu_type = (data_offset[offset] >> 1) & 0x3f;
+        if (H265_NAL_VPS == nalu_type || H265_NAL_SPS == nalu_type || H265_NAL_PPS == nalu_type)
+        {
+            args_->vps_sps_pps_flag = 1;
+        }
+        int irap = static_cast<int>((H265_NAL_BLA_W_LP <= nalu_type) && (nalu_type <= H265_NAL_RSV_IRAP));
+        if (irap && args_->vps_sps_pps_flag == 0)
+        {
+            int n = h265_vps_sps_pps_size(&args_->hevc);
+            auto frame = fixed_frame_buffer::create(data_offset, n);
+            frame->set_media(simple_rtmp::rtmp_tag::video);
+            frame->set_codec(simple_rtmp::rtmp_codec::h265);
+            frame->set_flag(keyframe);
+            frame->set_pts(timestamp + cts);
+            frame->set_dts(timestamp);
+            n = mpeg4_hevc_to_nalu(&args_->hevc, frame->data(), frame->size());
+            if (n <= 0)
+            {
+                return;
+            }
+            frame->resize(n);
+            offset += n;
+            on_frame(frame, {});
+            args_->vps_sps_pps_flag = 1;
+        }
+        static const uint8_t h265_start_code[] = {0x00, 0x00, 0x00, 0x01};
+        data_offset += offset;
+        auto frame = fixed_frame_buffer::create(data_offset, length);
+        frame->set_media(simple_rtmp::rtmp_tag::video);
+        frame->set_codec(simple_rtmp::rtmp_codec::h265);
+        frame->set_flag(keyframe);
+        frame->set_pts(timestamp + cts);
+        frame->set_dts(timestamp);
+        on_frame(frame, {});
+
+        offset += length;
+        data_offset += offset;
+    }
 }
