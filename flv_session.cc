5645080eb18718d41e80ef3ff51dd52d1fc1a0c6
gyl30 <im.ggyyll@gmail.com>
2023-11-01 22:25:45 +0800

    完善 flv_session

--- flv_session.cc
+++ flv_session.cc
@@ -1,21 +1,59 @@
+#include <boost/algorithm/string.hpp>
 #include "flv_session.h"
+#include "sink.h"
+#include "log.h"
 
 using simple_rtmp::flv_session;
 
-flv_session::flv_session(simple_rtmp::executors::executor& ex, boost::asio::ip::tcp::socket socket) : ex_(ex), conn_(std::make_shared<tcp_connection>(ex_, std::move(socket)))
+flv_session::flv_session(std::string target, simple_rtmp::executors::executor& ex, boost::asio::ip::tcp::socket socket)
+    : target_(std::move(target)), ex_(ex), conn_(std::make_shared<tcp_connection>(ex_, std::move(socket)))
 {
 }
 
 void flv_session::start()
 {
+    std::vector<std::string> results;
+    boost::split(results, target_, boost::is_any_of("/"));
+    if (results.size() != 3)
+    {
+        shutdown();
+        return;
+    }
+    std::string app = results[1];
+    std::string stream = results[2];
+    std::string id = "flv_" + app + "_" + stream;
+
+    auto s = sink::get(id);
+    if (!s)
+    {
+        LOG_ERROR("not found sink {}", id);
+        shutdown();
+        return;
+    }
+    sink_ = s;
+    s->add_channel(channel_);
+
+    channel_ = std::make_shared<simple_rtmp::channel>();
+    channel_->set_output(std::bind(&flv_session::channel_out, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+
     conn_->set_read_cb(std::bind(&flv_session::on_read, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
     conn_->set_write_cb(std::bind(&flv_session::on_write, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
     conn_->start();
 }
 
+void flv_session::channel_out(const frame_buffer::ptr& frame, const boost::system::error_code& ec)
+{
+    if (ec)
+    {
+        LOG_ERROR("channel out {} failed {}", static_cast<void*>(this), ec.message());
+        shutdown();
+        return;
+    }
+    conn_->write_frame(frame);
+}
+
 void flv_session::shutdown()
 {
-    //
     auto self = shared_from_this();
 
     ex_.post([this, self]() { safe_shutdown(); });
@@ -23,23 +61,39 @@ void flv_session::shutdown()
 
 void flv_session::safe_shutdown()
 {
-    //
+    auto s = sink_.lock();
+    if (s)
+    {
+        s->del_channel(channel_);
+    }
+    if (channel_)
+    {
+        channel_.reset();
+    }
 
-    conn_->shutdown();
-    conn_.reset();
+    if (conn_)
+    {
+        conn_->shutdown();
+        conn_.reset();
+    }
 }
 
 void flv_session::on_read(const simple_rtmp::frame_buffer::ptr& frame, boost::system::error_code ec)
 {
-    //
-}
-
-void flv_session::write_frame(const simple_rtmp::frame_buffer::ptr& frame)
-{
-    conn_->write_frame(frame);
+    (void)frame;
+    if (ec)
+    {
+        shutdown();
+        return;
+    }
 }
 
 void flv_session::on_write(const boost::system::error_code& ec, std::size_t bytes)
 {
-    //
+    (void)bytes;
+    if (ec)
+    {
+        shutdown();
+        return;
+    }
 }
--- flv_session.h
+++ flv_session.h
@@ -6,20 +6,20 @@
 #include <boost/beast.hpp>
 #include "http_session.h"
 #include "tcp_connection.h"
+#include "channel.h"
+#include "sink.h"
 
 namespace simple_rtmp
 {
 class flv_session : public std::enable_shared_from_this<flv_session>
 {
    public:
-    flv_session(simple_rtmp::executors::executor& ex, boost::asio::ip::tcp::socket socket);
+    flv_session(std::string target, simple_rtmp::executors::executor& ex, boost::asio::ip::tcp::socket socket);
     ~flv_session() = default;
 
    public:
     void start();
     void shutdown();
-
-    void write_frame(const simple_rtmp::frame_buffer::ptr& frame);
 
    private:
     void on_read(const simple_rtmp::frame_buffer::ptr& frame, boost::system::error_code ec);
@@ -27,6 +27,12 @@ class flv_session : public std::enable_shared_from_this<flv_session>
     void safe_shutdown();
 
    private:
+    void channel_out(const frame_buffer::ptr& frame, const boost::system::error_code& ec);
+
+   private:
+    std::string target_;
+    sink::weak sink_;
+    channel::ptr channel_ = nullptr;
     simple_rtmp::executors::executor& ex_;
     std::shared_ptr<tcp_connection> conn_;
 };
