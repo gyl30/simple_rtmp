8cb5dc6a1ea3d53796a4f7a32179c4eea3cd916c
gyl30 <im.ggyyll@gmail.com>
2023-11-03 22:35:48 +0800

    flv h264 继承 flv encoder

--- flv_h264_encoder.h
+++ flv_h264_encoder.h
@@ -1,25 +1,24 @@
-#ifndef SIMPLE_RTMP_FLV_ENCODER_H
-#define SIMPLE_RTMP_FLV_ENCODER_H
+#ifndef SIMPLE_RTMP_FLV_H264_ENCODER_H
+#define SIMPLE_RTMP_FLV_H264_ENCODER_H
 
-#include <set>
 #include <string>
 #include <memory>
-#include <functional>
-#include "frame_buffer.h"
 #include "channel.h"
+#include "frame_buffer.h"
+#include "flv_encoder.h"
 
 namespace simple_rtmp
 {
-class flv_h264_encoder
+class flv_h264_encoder : public flv_encoder
 {
    public:
     explicit flv_h264_encoder(std::string id);
-    ~flv_h264_encoder() = default;
+    ~flv_h264_encoder() override = default;
 
    public:
-    std::string id();
-    void write(const frame_buffer::ptr& frame, const boost::system::error_code& ec);
-    void set_output(const channel::ptr& ch);
+    std::string id() override;
+    void write(const frame_buffer::ptr& frame, const boost::system::error_code& ec) override;
+    void set_output(const channel::ptr& ch) override;
 
    private:
     void muxer_frame(const frame_buffer::ptr& frame, boost::system::error_code ec);
